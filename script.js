d0559e03d0d2f954b0c913356132256ce87e196a
JLC <jlc@annonymous.com>
2020-07-29 23:52:07 -0500

    Implementing questions json object array and starting function submit results

--- script.js
+++ script.js
@@ -3,68 +3,72 @@ const timer = document.getElementById("timer-count");
 const startBtn = document.getElementById("start-quiz");
 const startContainer = document.getElementById("start-container");
 const quizConatiner = document.getElementById("quiz-container");
+const questionsDiv = document.getElementById("answers");
+const answersDiv = document.getElementById("answers");
 const results = document.getElementById("results");
 ///////
 
 /* Counter variable to handle the timer */
 let countdown = 60;
 
-//////////* JSON object to store questions and their answers */
-let questions = {
-    "id": 1,
-    "question": "Whats the only way to create a variable in plain JavaScript?",
-    "answers": [
-        { "id": 1, "text": "const", "flag": false },
-        { "id": 2, "text": "let", "flag": false },
-        { "id": 3, "text": "var", "flag": false },
-        { "id": 4, "text": "All of the above", "flag": true },
-    ]
-}
+// Questions increment
+let countQuestions = 0;
 
-/* let question2 = {
-    "id": 2,
-    "question": "What is triple equals ' === ' checking for?",
-    "answers": [
-        { "id": 1, "text": "Compare data type and value", "flag": true },
-        { "id": 2, "text": "Assign a new value to a variable", "flag": false },
-        { "id": 3, "text": "Compare data type", "flag": false },
-        { "id": 4, "text": "Compare for value match", "flag": false },
-    ]
-} */
+//////////* JSON object array to store questions and their answers */
+let questions = [{
+        "id": 1,
+        "question": "Whats the only way to create a variable in plain JavaScript?",
+        "answers": [
+            { "id": 1, "text": "const", "flag": false },
+            { "id": 2, "text": "let", "flag": false },
+            { "id": 3, "text": "var", "flag": false },
+            { "id": 4, "text": "All of the above", "flag": true },
+        ]
+    },
+    {
+        "id": 2,
+        "question": "What is triple equals ' === ' checking for?",
+        "answers": [
+            { "id": 1, "text": "Compare data type and value", "flag": true },
+            { "id": 2, "text": "Assign a new value to a variable", "flag": false },
+            { "id": 3, "text": "Compare data type", "flag": false },
+            { "id": 4, "text": "Compare for value match", "flag": false },
+        ]
+    },
+    {
+        "id": 3,
+        "question": "What is the output of console.log(11 + '11')",
+        "answers": [
+            { "id": 1, "text": "11", "flag": false },
+            { "id": 2, "text": "1111", "flag": true },
+            { "id": 3, "text": "22", "flag": false },
+            { "id": 4, "text": "undefined", "flag": false },
+        ]
 
-/* let question3 = {
-    "id": 3,
-    "question": "What is the output of console.log(11 + "11")",
-    "answers": [
-        { "id": 1, "text": "11", "flag": false },
-        { "id": 2, "text": "1111", "flag": true },
-        { "id": 3, "text": "22", "flag": false },
-        { "id": 4, "text": "undefined", "flag": false },
-    ]
-} */
-
-/* let question4 = {
-    "id": 4,
-    "question": "Given the following array Fruits = ['Apples', 'Pears', 'Oranges', 'Bananas'] What is the value in the index position 3",
-    "answers": [
-        { "id": 1, "text": "Pears", "flag": false },
-        { "id": 2, "text": "Apples", "flag": false },
-        { "id": 3, "text": "Oranges", "flag": false },
-        { "id": 4, "text": "Bananas", "flag": true },
-    ]
-} */
-
-/* let question5 = {
-    "id": 5,
-    "question": "In javascript what is the array object built in method used to insert a new element into an array?",
-    "answers": [
-        { "id": 1, "text": "filter", "flag": false },
-        { "id": 2, "text": "pop", "flag": false },
-        { "id": 3, "text": "splice", "flag": false },
-        { "id": 4, "text": "push", "flag": true },
-    ]
-} */
-//////////
+    },
+    {
+        "id": 4,
+        "question": "Given the following array Fruits = ['Apples', 'Pears', 'Oranges', 'Bananas'] What is the value in the index position 3",
+        "answers": [
+            { "id": 1, "text": "Pears", "flag": false },
+            { "id": 2, "text": "Apples", "flag": false },
+            { "id": 3, "text": "Oranges", "flag": false },
+            { "id": 4, "text": "Bananas", "flag": true },
+        ]
+    },
+    {
+        "id": 5,
+        "question": "In javascript what is the array object built in method used to insert a new element into an array?",
+        "answers": [
+            { "id": 1, "text": "filter", "flag": false },
+            { "id": 2, "text": "pop", "flag": false },
+            { "id": 3, "text": "splice", "flag": false },
+            { "id": 4, "text": "push", "flag": true },
+        ]
+    },
+];
+console.log(questions.length);
+////////////////End of the JSON object array of the questions
 
 //////////* Printing questions to the console */
 function printQuestionsToConsole() {
@@ -77,30 +81,43 @@ function printQuestionsToConsole() {
 ///////////
 
 //////////* Printing questions to the DOM */
-function printQuestionsToDOM() {
-    quizConatiner.setAttribute("data-state", "show");
-    quizConatiner.setAttribute("style", "display:block;")
-    document.getElementById("questions").innerHTML = `<h1>Question # ${questions.id}</h1><p>${questions.question}</p>`;
-    for (let y = 0; y < questions.answers.length; y++) {
+function printQuestionsToDOM(index) {
+    questionsDiv.innerHTML = `<h1>Question # ${questions[index].id} of ${questions.length}</h1><p>${questions[index].question}</p>`;
+    for (let y = 0; y < questions[index].answers.length; y++) {
+        // Grabing li element in a variable to insert answer buttons
         let z = document.createElement("li");
-        z.innerHTML = `<button id="${questions.answers[y].id}" class="btn-answers" data-flag="${questions.answers[y].flag}">${questions.answers[y].text}</button>`;
+        z.innerHTML = `<button id="${questions[index].answers[y].id}" class="btn-answers" data-flag="${questions[index].answers[y].flag}">${questions[index].answers[y].text}</button>`;
+        // Adding eventListener click to each answer button rendered
         z.addEventListener("click", (e) => {
+            //reading the data-flag of each answer
             let flag = e.target.getAttribute("data-flag");
+            //evaluate if the answer clicked is incorrect 
             if (flag === "false") {
+                //substract time to the timer
                 countdown = countdown - 10;
+                // Display result to the user
                 results.innerHTML = `<div class="card card-body">
-                                        <h1>Incorrect</h1>
+                                        <h1>Incorrect -10</h1>
                                     </div>`;
-                return;
-            } else {
+                countQuestions++;
+                //Exit the function                    
+                //return;
+            } else { //evaluate if the answer clicked is correct
+                //Add time to the timer
                 countdown = countdown + 10;
+                // Display result to the user
                 results.innerHTML = `<div class="card card-body">
-                                        <h1>Correct</h1>
+                                        <h1>Correct +10</h1>
                                     </div>`;
-                return;
+                countQuestions++;
+                //Exit this function                                        
+                //return;
             }
-        })
-        document.getElementById("answers").append(z);
+
+        });
+        //Adding answers to the <li> elements
+        answersDiv.append(z);
+
     }
 }
 ///////////
@@ -115,7 +132,7 @@ function startTimer() {
         timer.textContent = `Your time left: ${countdown}`;
         if (countdown === 0) {
             clearInterval(interval);
-            //finishTest()
+            alert("Game Over");
             return;
         }
     }, 1000);
@@ -137,7 +154,18 @@ startBtn.addEventListener("click", (e) => {
     startTimer();
     //hide the start container
     hideStartingContainer();
-    //Present/show the questions
-    printQuestionsToDOM();
+    //show the questions
+    quizConatiner.setAttribute("data-state", "show");
+    quizConatiner.setAttribute("style", "display:block;");
+
+    //render questions
+    if (countQuestions < questions.length) {
+        printQuestionsToDOM(countQuestions);
+        console.log(countQuestions);
+    }
+
+    if (countdown === 0 || countQuestions === 5) {
+        alert("Game Over");
+    }
 });
 /////////
