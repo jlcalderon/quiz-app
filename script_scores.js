7251b5abd976defe7c3f51c8b41a6bc3f4957d0c
JLC <jlc@annonymous.com>
2020-07-31 13:02:00 -0500

    Add options with functionalities in scores dashboard

--- script_scores.js
+++ script_scores.js
@@ -1,5 +1,9 @@
 //Scores Dashboard
 const scoresDashboardText = document.getElementById("scores-board");
+//Dashboard options
+const gobackBtn = document.getElementById("btn-back");
+const clearBtn = document.getElementById("btn-clear");
+
 //get results from localstorage variables and push it into an array
 let testTakerResultLS = localStorage.getItem("test-taker-result");
 let testTakerLS = localStorage.getItem("test-taker-name");
@@ -12,14 +16,37 @@ let jsonResult = {
 renderScores();
 
 function renderScores() {
-    scoresDashboardText.innerHTML =
-        `<div class="row">
-            <div class="col-sm-6 col-md-6">
-                <p>${jsonResult.testtaker}</p>
+    if (jsonResult.testtaker != null && jsonResult.testscore != null) {
+        scoresDashboardText.innerHTML =
+            `<div class="row">
+                <div class="col-sm-6 col-md-6">
+                    <p>Test Taker Initials: ${jsonResult.testtaker}</p>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                    <p>Scores obtained: ${jsonResult.testscore}</p>
+                </div>
             </div>
-            <div class="col-sm-6 col-md-6">
-                <p>${jsonResult.testscore}</p>
-            </div>
-        </div>
-        <hr>`;
+            <hr>`;
+    } else {
+        scoresDashboardText.innerHTML = `<div class="row">
+                                        <div class="col-sm-12 col-md-12">
+                                            <p>No results to display, try going back and complete the test to submit your scores.</p>                  
+                                        </div>
+                                    </div>`;
+    }
 }
+
+// Go Back option
+gobackBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location = "./index.html";
+});
+
+// Clear scores option
+clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.removeItem("test-taker-result");
+    localStorage.removeItem("test-taker-name");
+    renderScores();
+    location.reload();
+})
